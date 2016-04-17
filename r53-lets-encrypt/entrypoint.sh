dc04479693fc8eb70c2959d2d59aea0d2bf0aac2
Euan Kemp <euank@euank.com>
2016-04-16 20:51:10 -0700

    Initial commit for route53 lets encrypt container

---
+++ r53-lets-encrypt/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+trap 'exit 0' INT TERM
+
+if [ -z "${LE_EMAIL}" ]; then
+  echo "We need an email for let's encrypt. Set env var LE_EMAIL"
+  exit 1
+fi
+
+if [ -z "${DOMAINS}" ]; then
+  echo "We need domains. Set env var DOMAINS"
+  exit 1
+fi
+
+[ -f /ssl/dhparam.pem ] || openssl dhparam -out ./certs/dhparam.pem 4096
+
+echo "${DOMAINS}" > domains.txt
+
+while true; do
+  ./letsencrypt.sh --config ./config.sh --cron
+  sleep 3600
+done
---
+++ r53-lets-encrypt/hook.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+set -x
+
+
+function get_zone {
+    local DOMAIN="${1}"
+    last_char=$(echo $DOMAIN | grep -o ".$")
+    if [ $last_char != "." ]; then
+        # Normalize "example.com" to "example.com." since all hosted zones have that format in name
+        DOMAIN="${DOMAIN}."
+    fi
+
+    zone=$(aws route53 list-hosted-zones | jq ".HostedZones[] | . as \$zone | select(\"${DOMAIN}\" | endswith(\$zone.Name)) | .Id" -r)
+    if [ -z "$zone" ]; then
+        echo "Failed to find zone id for $DOMAIN"
+        exit 1
+    fi
+
+    echo $zone
+}
+
+
+function deploy_challenge {
+    local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
+
+    # This hook is called once for every domain that needs to be
+    # validated, including any alternative names you may have listed.
+    #
+    # Parameters:
+    # - DOMAIN
+    #   The domain name (CN or subject alternative name) being
+    #   validated.
+    # - TOKEN_FILENAME
+    #   The name of the file containing the token to be served for HTTP
+    #   validation. Should be served by your web server as
+    #   /.well-known/acme-challenge/${TOKEN_FILENAME}.
+    # - TOKEN_VALUE
+    #   The token value that needs to be served for validation. For DNS
+    #   validation, this is what you want to put in the _acme-challenge
+    #   TXT record. For HTTP validation it is the value that is expected
+    #   be found in the $TOKEN_FILENAME file.
+
+    # Route53 DNS challange
+    zone=$(get_zone $DOMAIN) || exit 1
+    aws route53 change-resource-record-sets --hosted-zone-id "$zone" --change-batch "{\"Comment\": \"lets encrypt.sh\", \"Changes\": [{\"Action\": \"UPSERT\", \"ResourceRecordSet\": {\"Name\": \"_acme-challenge.${DOMAIN}\", \"Type\": \"TXT\", \"TTL\": 0, \"ResourceRecords\": [{\"Value\": \"\\\"${TOKEN_VALUE}\\\"\"}]}}]}"
+
+    # Takes some time for r53 records to propagate
+    sleep 30
+}
+
+
+function clean_challenge {
+    local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
+
+    # This hook is called after attempting to validate each domain,
+    # whether or not validation was successful. Here you can delete
+    # files or DNS records that are no longer needed.
+    #
+    # The parameters are the same as for deploy_challenge.
+
+    zone=$(get_zone $DOMAIN) || exit 1
+    aws route53 change-resource-record-sets --hosted-zone-id "$zone" --change-batch "{\"Comment\": \"lets encrypt.sh\", \"Changes\": [{\"Action\": \"DELETE\", \"ResourceRecordSet\": {\"Name\": \"_acme-challenge.${DOMAIN}\", \"Type\": \"TXT\", \"TTL\": 0, \"ResourceRecords\": [{\"Value\": \"\\\"${TOKEN_VALUE}\\\"\"}]}}]}"
+}
+
+function deploy_cert {
+    local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" CHAINFILE="${4}"
+
+    # This hook is called once for each certificate that has been
+    # produced. Here you might, for instance, copy your new certificates
+    # to service-specific locations and reload the service.
+    #
+    # Parameters:
+    # - DOMAIN
+    #   The primary domain name, i.e. the certificate common
+    #   name (CN).
+    # - KEYFILE
+    #   The path of the file containing the private key.
+    # - CERTFILE
+    #   The path of the file containing the signed certificate.
+    # - CHAINFILE
+    #   The path of the file containing the full certificate chain.
+    cp $KEYFILE /certs/ssl.key;
+    cat $CHAINFILE $CERTFILE > /certs/ssl.pem;
+    # Docker runs as root... I'm so sorry :(
+    chmod 444 /certs/ssl.{key,pem}
+}
+
+HANDLER=$1; shift; $HANDLER $@
